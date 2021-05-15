0229bb320b0bc214d649ba2d2e30bacda5a839d6
Zubair <57180013+MZubairLohar@users.noreply.github.com>
2021-05-15 13:23:27 +0500

    finished

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import {Loading, Tours} from './Components/index'
+
+//url
+const url = 'https://course-api.com/react-tours-project';
 
 function App() {
-  return (
-    <div className="app">
-      <h3>Boiler Plate Ready </h3>
-    </div>
-  );
+  const [loading , setLoading] = useState(true);
+  const [tours , setTours] = useState([]);
+
+  const removeTour = (id)=>{
+      const newTour = tours.filter((tour)=> tour.id !== id);
+      setTours(newTour);   
+  }
+  
+
+  const fetchData = async () =>{
+    setLoading(true)
+
+    try {  
+      const response = await fetch(url);
+      const tours = await response.json()
+      setLoading(false);
+      setTours(tours);
+    
+    } catch (error) {
+      setLoading(false);
+      alert(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, [])
+
+  if(loading){
+    <main>
+      <Loading />
+    </main>
+  }
+
+  return(
+    <main>
+      <Tours tours = {tours} removeTour={removeTour} />
+    </main>
+  )
 }
 
 export default App;
---
+++ src/Components/Loading.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import loader from './assests/loader.gif'
+const Loading = () => {
+    return (
+        <div>
+            <img src={loader} alt='loading...'/>
+        </div>
+    )
+}
+
+export default Loading
